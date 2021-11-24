83ff0c2731b4be419686a9f501725930ad6b7151
GCH190876 <ducbvmgch190876@fpt.edu.vn>
2021-11-24 11:07:49 +0700

    Update 24/11

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 
 app.set('view engine','hbs')
 
+app.use(express.urlencoded({extended:true}))
+
 app.get('/',(req,res)=>{
     res.render('index')
 })
@@ -10,11 +12,11 @@ app.get('/',(req,res)=>{
 const adminController = require('./admin')
 app.use('/admin',adminController)
 
-const staffController = require('./staff')
-app.use('/staff',staffController)
+// const staffController = require('./staff')
+// app.use('/staff',staffController)
 
-const trainerController = require('./trainer')
-app.use('/trainer',trainerController)
+// const trainerController = require('./trainer')
+// app.use('/trainer',trainerController)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
---
+++ databaseHandler.js
@@ -0,0 +1,18 @@
+const {MongoClient,ObjectId} = require('mongodb');
+
+const URL = 'mongodb+srv://atn:atn123456@cluster0.ajh2g.mongodb.net/test';
+const DATABASE_NAME = "myDatabase"
+
+async function getDB() {
+    const client = await MongoClient.connect(URL);
+    const dbo = client.db(DATABASE_NAME);
+    return dbo;
+}
+
+async function insertObject(collectionName,objectToInsert){
+    const dbo = await getDB();
+    const newObject = await dbo.collection(collectionName).insertOne(objectToInsert);
+    console.log("Gia tri id moi duoc insert la: ", newObject.insertedId.toHexString());
+}
+
+module.exports = {insertObject}
